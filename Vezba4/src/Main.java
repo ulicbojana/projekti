4f7270cdb42098386f74942380862f7586876d13
ulicbojana <ulicbojana@yahoo.com>
2017-05-16 20:16:54 +0200

    dodali smo vezbu 4

---
+++ Vezba4/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+		 public static void main(String[] args) {
+		        System.out.print("Rec: ");
+		        Scanner unos = new Scanner(System.in);
+		        String rec= unos.nextLine();
+		        for(int i=0;i<10;i++){
+		        	
+		        	 System.out.println( rec);
+		        }
+		 
+		       
+		    }
+		}
+
+	
+
+
